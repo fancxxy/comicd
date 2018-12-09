651c795b081f4d9703d87e19a27709459ad9b0f3
fancxxy <fancxxy@gmail.com>
2018-12-09 17:59:43 +0800

    fix issue for tencent moudle

--- comicd/interface/tencent.py
+++ comicd/interface/tencent.py
@@ -5,6 +5,7 @@ from base64 import b64decode
 from json import loads
 from re import compile, findall, sub
 from comicd.interface import Web
+import execjs
 
 
 class Tencent(Web):
@@ -12,15 +13,15 @@ class Tencent(Web):
     host = ['ac.qq.com', 'manhua.qpic.cn']
 
     _pattern = {
-        'comic_url': compile(r'^http://ac.qq.com/Comic/[Cc]omicInfo/id/(.+)/?$'),
-        'chapter_url': compile(r'^http://ac.qq.com/ComicView/index/id/(\d+)/cid/\d+/?$'),
+        'comic_url': compile(r'^https://ac.qq.com/Comic/[Cc]omicInfo/id/(.+)/?$'),
+        'chapter_url': compile(r'^https://ac.qq.com/ComicView/index/id/(\d+)/cid/\d+/?$'),
         'summary': compile(r'(?s)<p class="works-intro-short ui-text-gray9">\s+(.*?)</p>'),
 
         'title': compile(r'<h2 class="works-intro-title ui-left"><strong>(.*?)</strong></h2>'),
         'scope': compile(r'(?s)<div class="works-chapter-list-wr ui-left">(.*?)</ol>'),
         'chapters': compile(r'<a target="_blank" title=[\'\"][^：]+：(.*?)[\'\"] href=[\'\"](.*?)[\'\"]>'),
         'data': compile(r'var\s+DATA\s+=\s+\'(.*?)\''),
-        'nonce': compile(r'window\.nonce = \'\' \+ \'(.*?)\''),
+        'nonce': compile(r'window\[.*?\]\s+=\s(.*?);'),
 
         'cover': compile(r'<img src="(.*?)" alt=".*?" height="280" width="210"/>'),
         'update': compile(r'''(?x)<span\ class="ui-font-fb">最新话：</span><a\ class="works-ft-new"\ href="(.*?)">\[(.*?)\]
@@ -67,7 +68,7 @@ class Tencent(Web):
         try:
             # abandon second value
             newest_url, newest_title, _, newest_date = self._pattern['update'].findall(content)[0]
-            return 'http://ac.qq.com' + newest_url, newest_title, newest_date.replace('.', '-')
+            return 'https://ac.qq.com' + newest_url, newest_title, newest_date.replace('.', '-')
         except AttributeError:
             return '', '', ''
 
@@ -76,7 +77,7 @@ class Tencent(Web):
         try:
             scope = self._pattern['scope'].search(content).group(1)
             chapters = self._pattern['chapters'].findall(scope)
-            return [(c[0], 'http://ac.qq.com' + c[1]) for c in chapters]
+            return [(c[0], 'https://ac.qq.com' + c[1]) for c in chapters]
         except AttributeError:
             return []
 
@@ -94,7 +95,6 @@ class Tencent(Web):
 
     def images(self, data):
         try:
-            # return [(p['pid'] + '.jpg', p['url']) for p in data[1]['picture']]
             count, result = 1, []
             for p in data[1]['picture']:
                 result.append(('{:0>3}.jpg'.format(count), p['url']))
@@ -110,13 +110,16 @@ class Tencent(Web):
         except AttributeError:
             return {}
 
+        nonce = execjs.eval(nonce)
         nonce = findall(r'\d+[a-zA-Z]+', nonce)
+        chars = list(data)
         for n in nonce[::-1]:
             locate = int(sub('\D', '', n)) & 255
             string = sub('\d', '', n)
-            data = data[:locate] + data[locate+len(string):]
+            del chars[locate:locate+len(string)]
 
         try:
+            data = ''.join(chars)
             return loads(b64decode(data).decode('utf8'))
         except UnicodeDecodeError:
             return {}
